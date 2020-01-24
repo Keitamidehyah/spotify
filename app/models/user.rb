4a9499f1e3f48ba59c227152b2e2e27de02b7062
Keitamidehyah <56815486+Keitamidehyah@users.noreply.github.com>
2020-01-24 14:43:01 +0900

    create-revies-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  def change
+    create_table :users do |t|
+      t.string :nickname
+      t.string :email
+      t.string :password
+      t.string :introduction
+      t.string :profimage
+      t.string :account
+  
+      t.timestamps
+    end
+  end
 end
--- db/migrate/20200124051252_create_users.rb
+++ db/migrate/20200124051252_create_users.rb
@@ -1,9 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :nickname
       t.string :email
-
+      t.string :password
+      t.string :introduction
+      t.string :profimage
+      t.string :account
+  
       t.timestamps
     end
   end
